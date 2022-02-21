58d089e49814abd456074667b7b635e6532947c6
Reinis Melbardis <rmelbardis@gmail.com>
2022-02-21 14:45:54 +0000

    simplified test

--- tests/test_preproc_text.py
+++ tests/test_preproc_text.py
@@ -3,4 +3,4 @@ from toolbox import preproc_text
 
 def test_preproc_text():
     assert preproc_text.preprocess_text(pd.Series('This. is. a. 42 test. strings!')).values[0] \
-       == pd.Series('test string').values[0]
+       == 'test string'
